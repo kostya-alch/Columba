1db3bfeb5d84a8b81668ef119cb72e1d47c27dbc
kostya-alch <kostafrompiter@gmail.com>
2021-05-07 16:08:55 +0300

    Исправлена критическая утечка памяти в ContactsFragment.kt

--- app/src/main/java/ui/fragments/ContactsFragment.kt
+++ app/src/main/java/ui/fragments/ContactsFragment.kt
@@ -21,71 +21,80 @@ import kotlinx.android.synthetic.main.fragment_contacts.*
 
 class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
 
-    private lateinit var mRecyclerView:RecyclerView
-    private lateinit var mAdapter:FirebaseRecyclerAdapter<CommonModel,ContactsHolder>
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mAdapter: FirebaseRecyclerAdapter<CommonModel, ContactsHolder>
     private lateinit var mRefContacts: DatabaseReference
     private lateinit var mRefUsers: DatabaseReference
-
-
+    private lateinit var mRefUsersListener:AppValueEventListener
+    private  var mapListeners = hashMapOf<DatabaseReference,AppValueEventListener>()
 
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title = "Контакты"
         initRecycleView()
-
-
     }
 
     private fun initRecycleView() {
         mRecyclerView = contacts_recycle_view
         mRefContacts = REF_DATABASE_ROOT.child(NODE_PHONES_CONTACTS).child(CURRENT_UID)
+
+        //Настройка для адаптера, где указываем какие данные и откуда получать
         val options = FirebaseRecyclerOptions.Builder<CommonModel>()
             .setQuery(mRefContacts, CommonModel::class.java)
             .build()
-        mAdapter = object :FirebaseRecyclerAdapter<CommonModel, ContactsHolder>(options){
+
+        //Адаптер принимает данные, отображает в холдере
+        mAdapter = object : FirebaseRecyclerAdapter<CommonModel, ContactsHolder>(options) {
 
             override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactsHolder {
-                val view = LayoutInflater.from(parent.context).inflate(R.layout.contact_item, parent, false)
+                //Запускается тогда когда адаптер получает доступ к ViewGroup
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.contact_item, parent, false)
                 return ContactsHolder(view)
             }
 
+            // Заполняет holder
             override fun onBindViewHolder(
                 holder: ContactsHolder,
                 position: Int,
                 model: CommonModel
             ) {
                 mRefUsers = REF_DATABASE_ROOT.child(NODE_USERS).child(model.id)
-                mRefUsers.addValueEventListener(AppValueEventListener{
+
+                mRefUsersListener = AppValueEventListener {
                     val contact = it.getCommonModel()
-
-
                     holder.name.text = contact.fullname
                     holder.status.text = contact.state
                     holder.photo.downloadAndSetImage(contact.photoUrl)
+                }
 
-                })
-
-
+                mRefUsers.addValueEventListener(mRefUsersListener)
+                mapListeners[mRefUsers] = mRefUsersListener
             }
-
         }
 
         mRecyclerView.adapter = mAdapter
         mAdapter.startListening()
+
     }
 
+    // Холдер для захвата ViewGroup
     class ContactsHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val name: TextView =  view.contact_fullname
+        val name: TextView = view.contact_fullname
         val status: TextView = view.contact_status
         val photo: CircleImageView = view.contact_photo
     }
 
     override fun onPause() {
         super.onPause()
-        mAdapter.startListening()
+        mAdapter.stopListening()
+        println()
+        mapListeners.forEach {
+            it.key.removeEventListener(it.value)
+        }
+        println()
     }
 }
-
 
 
 
